1d731f31aab1bef4c794984ba18779cf1f9f7907
Huda Yousif <hjdoski@gmail.com>
2019-10-02 22:41:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,10 @@
 def find_element_index(array, value_to_find)
-  array.each_index
+  array.length.times do |count|
+     if array[count] == value_to_find
+      return count 
+    end 
+    nil 
+  end
 end
 
 def find_max_value(array)
